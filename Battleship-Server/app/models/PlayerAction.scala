d754bfadb0f35f1add57022048391bbef3e3a53c
Sebastian Mader <basti.mader@googlemail.com>
2015-02-08 11:46:21 +0100

    coordinates to stringlist

--- Battleship-Server/app/models/PlayerAction.scala
+++ Battleship-Server/app/models/PlayerAction.scala
@@ -12,8 +12,8 @@ class PlayerAction(val coords :(Int, Int), val hitType :HitTypes, val shipType :
 
     def toJSONObject :JSONObject = {
       shipType match{
-        case None => JSONObject(Map("coordinates" -> new JSONArray(List[Any](coords._1, (coords._2 + 65).asInstanceOf[Char])), "type" -> hitType.toString))
-        case _ => new JSONObject(Map("coordinates" -> new JSONArray(List[Any](coords._1, (coords._2 + 65).asInstanceOf[Char])), "type" -> hitType.toString, "shiptype" -> shipType.get))
+        case None => JSONObject(Map("coordinates" -> new JSONArray(List[String](coords._1.toString, (coords._2 + 65).asInstanceOf[Char].toString)), "type" -> hitType.toString))
+        case _ => new JSONObject(Map("coordinates" -> new JSONArray(List[String](coords._1.toString, (coords._2 + 65).asInstanceOf[Char].toString)), "type" -> hitType.toString, "shiptype" -> shipType.get))
       }
     }
 }
